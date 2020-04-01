6d41814004d5116ba21041a010f9abe22a3410f6
Oscar Marshall <oscar.lim.marshall@gmail.com>
2020-04-01 05:31:58 -0700

    Add .doom.d

---
+++ .doom.d/packages.el
@@ -0,0 +1,15 @@
+;; -*- no-byte-compile: t; -*-
+;;; ~/.doom.d/packages.el
+
+;;; Examples:
+;; (package! some-package)
+;; (package! another-package :recipe (:fetcher github :repo "username/repo"))
+;; (package! builtin-package :disable t)
+
+(package! copy-as-format)
+(package! crux)
+(package! flycheck-clj-kondo)
+(package! flycheck-joker :disable t)
+(package! highlight-symbol)
+(package! html-to-hiccup)
+(package! super-save)
