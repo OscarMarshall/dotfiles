1218e7da63fbfac4a92309ed08b28b7b8874ccad
Oscar Marshall <oscar.lim.marshall@gmail.com>
2025-12-26 15:09:25 -0800

    Update profilarr.nix

--- modules/profilarr.nix
+++ modules/profilarr.nix
@@ -3,9 +3,7 @@
     image = "santiagosayshey/profilarr:latest";
     ports = ["127.0.0.1:6868:6868"];
     volumes = ["/metalminds/profilarr:/config"];
-    environment = {
-      TZ = config.time.timeZone;
-    };
+    environment.TZ = config.time.timeZone;
   };
 
   services.nginx.virtualHosts."profilarr.harmony.silverlight-nex.us" = {
