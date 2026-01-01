667326a3cc492c5923b8c47cc1766020f87250dc
Oscar Marshall <oscar.lim.marshall@gmail.com>
2026-01-01 02:54:44 -0800

    Update gnome.nix

--- modules/gnome.nix
+++ modules/gnome.nix
@@ -1,21 +1,27 @@
 {pkgs, ...}: {
-  # GNOME Desktop Environment
-  services.displayManager.gdm.enable = true;
-  services.desktopManager.gnome.enable = true;
-  environment.gnome.excludePackages = with pkgs; [
-    epiphany
-    gnome-calendar
-    gnome-console
-    gnome-contacts
-    gnome-maps
-    gnome-tour
-    gnome-user-docs
-    gnome-weather
-  ];
+  environment = {
+    gnome.excludePackages = with pkgs; [
+      epiphany
+      gnome-calendar
+      gnome-console
+      gnome-contacts
+      gnome-maps
+      gnome-tour
+      gnome-user-docs
+      gnome-weather
+    ];
+
+    systemPackages = with pkgs; [
+      # Enable system tray icons
+      gnomeExtensions.appindicator
+    ];
+  };
+
+  # Automatic screen rotation
   hardware.sensor.iio.enable = true;
 
-  # GNOME Extensions
-  environment.systemPackages = with pkgs; [
-    gnomeExtensions.appindicator
-  ];
+  services = {
+    desktopManager.gnome.enable = true;
+    displayManager.gdm.enable = true;
+  };
 }
