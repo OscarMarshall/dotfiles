0874da92f9201a876058e86ff9ae3ae87bd9b944
Oscar Marshall <oscar.lim.marshall@gmail.com>
2026-01-01 02:49:16 -0800

    Update boot.nix

--- modules/boot.nix
+++ modules/boot.nix
@@ -1,11 +1,11 @@
 {pkgs, ...}: {
   boot = {
     kernelModules = ["coretemp"];
+    # Use latest kernel for all systems
+    kernelPackages = pkgs.linuxPackages_latest;
     loader = {
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
     };
-    # Use latest kernel for all systems
-    kernelPackages = pkgs.linuxPackages_latest;
   };
 }
