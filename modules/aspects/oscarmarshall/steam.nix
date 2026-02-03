12db09ce20c251a2931811fa83b5609bdc3f902e
Oscar Marshall <3111765+OscarMarshall@users.noreply.github.com>
2026-02-02 23:10:26 -0800

    Fix Steam again

--- modules/aspects/oscarmarshall/steam.nix
+++ modules/aspects/oscarmarshall/steam.nix
@@ -1,7 +1,13 @@
 { den, ... }:
 {
   oscarmarshall.steam = {
-    includes = [ (den._.unfree [ "steam" ]) ];
+    includes = [
+      (den._.unfree [
+        "steam"
+        "steam-unwrapped"
+      ])
+    ];
+
     darwin.homebrew.casks = [ "steam" ];
     nixos.programs.steam.enable = true;
   };
