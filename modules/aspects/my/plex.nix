d26b231968328c7b4cda0889b0623f4c91669387
Oscar Marshall <3111765+OscarMarshall@users.noreply.github.com>
2026-02-16 20:04:24 -0800

    Put includes in the right place

--- modules/aspects/my/plex.nix
+++ modules/aspects/my/plex.nix
@@ -1,17 +1,19 @@
 { den, ... }:
 {
-  my.plex.nixos = {
+  my.plex = {
     includes = [ (den._.unfree [ "plexmediaserver" ]) ];
 
-    services.plex = {
-      enable = true;
-      openFirewall = true;
-    };
+    nixos = {
+      services.plex = {
+        enable = true;
+        openFirewall = true;
+      };
 
-    services.nginx.virtualHosts."plex.harmony.silverlight-nex.us" = {
-      forceSSL = true;
-      enableACME = true;
-      locations."/".proxyPass = "http://127.0.0.1:32400/";
+      services.nginx.virtualHosts."plex.harmony.silverlight-nex.us" = {
+        forceSSL = true;
+        enableACME = true;
+        locations."/".proxyPass = "http://127.0.0.1:32400/";
+      };
     };
   };
 }
