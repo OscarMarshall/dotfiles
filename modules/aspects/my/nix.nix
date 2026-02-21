0aa86cbaa29424cf2025c331de1176ea96324d1e
Oscar Marshall <3111765+OscarMarshall@users.noreply.github.com>
2026-02-20 23:25:48 -0800

    Set up Cachix (#118)

--- modules/aspects/my/nix.nix
+++ modules/aspects/my/nix.nix
@@ -14,8 +14,14 @@ let
             "nix-command"
             "flakes"
           ];
-          substituters = [ "https://nix-community.cachix.org" ];
-          trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
+          substituters = [
+            "https://nix-community.cachix.org"
+            "https://oscarmarshall.cachix.org"
+          ];
+          trusted-public-keys = [
+            "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+            "oscarmarshall.cachix.org-1:Fa13vGeBXoJ7jWpvnalg/PCRTtvCpyuHUFL5jQXt/9w="
+          ];
         };
       };
     };
