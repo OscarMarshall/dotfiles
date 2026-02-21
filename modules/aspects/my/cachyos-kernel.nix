aaf07bb733e58b96607eeff35d24e5e2a90103e7
Oscar Marshall <3111765+OscarMarshall@users.noreply.github.com>
2026-02-21 00:12:54 -0800

    Update cachyos-kernel.nix

--- modules/aspects/my/cachyos-kernel.nix
+++ modules/aspects/my/cachyos-kernel.nix
@@ -60,8 +60,8 @@
           };
 
           nix.settings = {
-            substituters = [ "https://attic.xuyh0120.win/lantian" ];
-            trusted-public-keys = [ "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc=" ];
+            extra-substituters = [ "https://attic.xuyh0120.win/lantian" ];
+            extra-trusted-public-keys = [ "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc=" ];
           };
         };
     };
