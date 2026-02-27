e3bcd9bd48f3807eb5bc09f055a3d6ff055d4c58
Oscar Marshall <3111765+OscarMarshall@users.noreply.github.com>
2026-02-26 17:30:16 -0800

    Update stylix.nix

--- modules/aspects/my/stylix.nix
+++ modules/aspects/my/stylix.nix
@@ -5,7 +5,7 @@ let
     {
       enable = true;
       base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
-      image = config.lib.stylix.pixel "base0A";
+      image = config.lib.stylix.pixel "base00";
     };
 in
 {
