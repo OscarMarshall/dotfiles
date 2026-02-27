60ae006b36b2cc5472899adacba39cd6af8b256a
Oscar Marshall <3111765+OscarMarshall@users.noreply.github.com>
2026-02-26 17:30:57 -0800

    Update catppuccin.nix

--- modules/aspects/my/catppuccin.nix
+++ modules/aspects/my/catppuccin.nix
@@ -1,26 +1,15 @@
-{ ... }:
 {
   my.catppuccin =
     {
       flavor ? "mocha",
     }:
-    let
-      base00 =
-        {
-          latte = "eff1f5";
-          frappe = "303446";
-          macchiato = "24273a";
-          mocha = "1e1e2e";
-        }
-        .${flavor};
-    in
     {
       homeManager =
         { config, pkgs, ... }:
         {
           stylix = {
             base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-${flavor}.yaml";
-            image = config.lib.stylix.pixel base00;
+            image = config.lib.stylix.pixel "base00";
             targets.emacs.colors.enable = false;
             targets.ghostty.colors.enable = false;
           };
