baa5d9868ea96f90677e51e644aee586d240f390
Oscar Marshall <3111765+OscarMarshall@users.noreply.github.com>
2026-03-09 16:12:30 -0700

    Update defaults.nix

--- modules/aspects/defaults.nix
+++ modules/aspects/defaults.nix
@@ -1,36 +1,37 @@
 {
   config,
   den,
-  lib,
   my,
   ...
 }:
 {
-  den.default.homeManager.programs.home-manager.enable = true;
+  den = {
+    schema.user.classes = [ "homeManager" ];
 
-  den.schema.user.classes = lib.mkDefault [ "homeManager" ];
+    ctx = {
+      host.includes = with my; [
+        fonts
+        nix
+        stylix
+    
+        # Disable booting when running on CI on all NixOS hosts.
+        (if config ? _module.args.CI then my.ci-no-boot else { })
+    
+        (
+          { host }:
+          {
+            nixos.networking.hostName = host.hostName;
+          }
+        )
+      ];
 
-  den.ctx.host.includes = with my; [
-    fonts
-    nix
-    stylix
-
-    # Disable booting when running on CI on all NixOS hosts.
-    (if config ? _module.args.CI then my.ci-no-boot else { })
-
-    (
-      { host }:
-      {
-        nixos.networking.hostName = host.hostName;
-      }
-    )
-  ];
-
-  den.ctx.user.includes = [
-    # ${user}.provides.${host} and ${host}.provides.${user}
-    my.routes
-
-    # Automatically create the user on host.
-    den._.define-user
-  ];
+      user.includes = with my; [
+        # ${user}.provides.${host} and ${host}.provides.${user}
+        routes
+    
+        # Automatically create the user on host.
+        den._.define-user
+      ];
+    };
+  };
 }
