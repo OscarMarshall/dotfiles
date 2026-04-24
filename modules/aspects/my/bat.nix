50421d61888b3ee1e6117c87155de5e1b828897c
Oscar Marshall <3111765+OscarMarshall@users.noreply.github.com>
2026-04-23 20:29:14 -0700

    Update bat.nix

--- modules/aspects/my/bat.nix
+++ modules/aspects/my/bat.nix
@@ -1,26 +1,10 @@
 {
   my.bat.homeManager =
     { pkgs, ... }:
-    let
-      nocheck = pkg: pkg.overrideAttrs (_: {
-        doCheck = false;
-        nativeCheckInputs = [ ];
-      });
-    in
     {
       programs.bat = {
         enable = true;
-        extraPackages = map nocheck (
-          with pkgs.bat-extras;
-          [
-            batdiff
-            batgrep
-            batman
-            batpipe
-            batwatch
-            prettybat
-          ]
-        );
+        extraPackages = [ pkgs.bat-extrasbat-extras ];
       };
     };
 }
