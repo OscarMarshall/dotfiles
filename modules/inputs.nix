fe90ed5c3f386ea8020cd3510133c6d7c12f6d9f
Oscar Marshall <3111765+OscarMarshall@users.noreply.github.com>
2026-06-19 04:33:29 -0700

    nixpkgs-unstable

--- modules/inputs.nix
+++ modules/inputs.nix
@@ -10,6 +10,8 @@
 { ... }:
 {
   flake-file.inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
