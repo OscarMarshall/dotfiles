c74e5d74302376ffc7c841009702f78c4427eeac
Oscar Marshall <3111765+OscarMarshall@users.noreply.github.com>
2026-06-30 02:12:47 -0700

    Update direnv.nix

--- modules/aspects/my/direnv.nix
+++ modules/aspects/my/direnv.nix
@@ -1,17 +1,6 @@
 {
-  my.direnv = {
-    homeManager = {
-      programs.direnv = {
-        enable = true;
-        nix-direnv.enable = true;
-      };
-    };
-
-    hmDarwin = { pkgs, ... }: {
-      programs.direnv.package = pkgs.direnv.overrideAttrs (_: {
-        doCheck = false;
-        nativeCheckInputs = [ ];
-      });
-    };
+  my.direnv.homeManager.programs.direnv = {
+    enable = true;
+    nix-direnv.enable = true;
   };
 }
